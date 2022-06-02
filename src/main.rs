0d4e0c4cd1caace83bcb4f5fa662414350007779
Mihail Antonov <antonovma@outlook.com>
2022-06-02 20:27:40 +0400

    First commit

---
+++ src/main.rs
@@ -0,0 +1,57 @@
+#![allow(unused)]
+extern crate gio;
+extern crate gtk;
+
+use std::env;
+
+use gio::{ApplicationExt, ApplicationExtManual, ApplicationFlags};
+use gtk::{
+    Application, ApplicationWindow, ContainerExt, GtkWindowExt, Inhibit, SeparatorToolItem,
+    ToolButton, ToolButtonExt, Toolbar, WidgetExt,
+};
+
+const PLAY_STOCK: &str = "gtk-media-play";
+
+fn main() {
+    let application = Application::new("com.github.rust-by-example", ApplicationFlags::empty())
+        .expect("Application initialization failed");
+    application.connect_startup(|application| {
+        // type inference is available for closures but not for functions
+        let window = ApplicationWindow::new(&application);
+        window.set_title("My first GUI");
+        window.show();
+        window.connect_delete_event(|_, _| Inhibit(false));
+        let toolbar = Toolbar::new();
+        window.add(&toolbar);
+        let open_button = ToolButton::new_from_stock("gtk-open");
+        toolbar.add(&open_button);
+
+        toolbar.add(&SeparatorToolItem::new());
+
+        let previous_button = ToolButton::new_from_stock("gtk-media-previous");
+        toolbar.add(&previous_button);
+
+        let play_button = ToolButton::new_from_stock(PLAY_STOCK);
+        toolbar.add(&play_button);
+
+        let stop_button = ToolButton::new_from_stock("gtk-media-stop");
+        toolbar.add(&stop_button);
+
+        let next_button = ToolButton::new_from_stock("gtk-media-next");
+        toolbar.add(&next_button);
+
+        toolbar.add(&SeparatorToolItem::new());
+
+        let remove_button = ToolButton::new_from_stock("gtk-remove");
+        toolbar.add(&remove_button);
+
+        toolbar.add(&SeparatorToolItem::new());
+
+        let quit_button = ToolButton::new_from_stock("gtk-quit");
+        toolbar.add(&quit_button);
+
+        window.show_all();
+    }); // Creates window
+    application.connect_activate(|_| {});
+    application.run(&env::args().collect::<Vec<_>>()); // starts the gtk event loop
+}
