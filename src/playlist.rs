95f58f35370c5f26a849bb076967561b53c8e6c1
Mihail Antonov <antonovma@outlook.com>
2022-06-03 15:43:51 +0400

    add mp3 metadata

--- src/playlist.rs
+++ src/playlist.rs
@@ -102,4 +102,47 @@ impl Playlist {
             pixbuf_loader.close().unwrap();
         }
     }
+    pub fn add(&self, path: &Path) {
+        // convert filename to string
+        let filename = path
+            .file_stem()
+            .unwrap_or_default()
+            .to_str()
+            .unwrap_or_default();
+        let row = self.model.append();
+        if let Ok(tag) = Tag::read_from_path(path) {
+            let title = tag.title().unwrap_or(filename); // if file contains no song title, file name will be displayed instead
+            let artist = tag.artist().unwrap_or("(no artist)");
+            let album = tag.album().unwrap_or("(no album)");
+            let genre = tag.genre().unwrap_or("(no genre)");
+            let year = tag
+                .year()
+                .map(|year| year.to_string())
+                .unwrap_or("(no year)".to_string());
+            let track = tag
+                .track()
+                .map(|track| track.to_string())
+                .unwrap_or("??".to_string());
+            let total_tracks = tag
+                .total_tracks()
+                .map(|total_tracks| total_tracks.to_string())
+                .unwrap_or("??".to_string());
+            let track_value = format!("{} / {}", track, total_tracks);
+            self.set_pixbuf(&row, &tag);
+
+            self.model.set_value(&row, TITLE_COLUMN, &title.to_value());
+            self.model
+                .set_value(&row, ARTIST_COLUMN, &artist.to_value());
+            self.model.set_value(&row, ALBUM_COLUMN, &album.to_value());
+            self.model.set_value(&row, GENRE_COLUMN, &genre.to_value());
+            self.model.set_value(&row, YEAR_COLUMN, &year.to_value());
+            self.model
+                .set_value(&row, TRACK_COLUMN, &track_value.to_value());
+        } else {
+            self.model
+                .set_value(&row, TITLE_COLUMN, &filename.to_value());
+        }
+        let path = path.to_str().unwrap_or_default();
+        self.model.set_value(&row, PATH_COLUMN, &path.to_value());
+    }
 }
