4fcc27d5d737e29f408c5b111e7de69b69a70b48
Mihail Antonov <antonovma@outlook.com>
2022-06-03 10:44:52 +0400

    works; cover added

--- src/main.rs
+++ src/main.rs
@@ -20,21 +20,42 @@ const PLAY_STOCK: &str = "gtk-media-play";
 const PAUSE_STOCK: &str = "gtk-media-pause";
 
 struct App {
+    adjustment: Adjustment,
+	cover: Image,
     toolbar: MusicToolbar,
     window: ApplicationWindow,
 }
 
-// this constructor Creates window and MusicToolbar
+// this constructor creates window and MusicToolbar
 impl App {
     fn new(application: Application) -> Self {
         let window = ApplicationWindow::new(&application);
-        window.set_title("Rusic");
+        window.set_title("Works");
         let toolbar = MusicToolbar::new();
-        // window.add(toolbar.toolbar()); // We remove this call because we'll instead add the toolbar to the box and the box to the window
+
         let vbox = gtk::Box::new(Vertical, 0);
         window.add(&vbox);
+
+// toolbar widgets
+        let toolbar = MusicToolbar::new();
+        vbox.add(toolbar.toolbar());
+        let cover = Image::new();
+        cover.set_from_file("src/image/atpharkfall.jpg");
+        vbox.add(&cover);
+// cursor widget
+        let adjustment = Adjustment::new(0.0, 0.0, 10.0, 0.0, 0.0, 0.0);
+        let scale = Scale::new(Horizontal, &adjustment);
+        scale.set_draw_value(false);
+        vbox.add(&scale);
+
         window.show_all();
-        let app = App { toolbar, window };
+		let app = App {
+			adjustment,
+			cover,
+			toolbar,
+			window,
+		};
+
         app.connect_events();
         app.connect_toolbar_events();
         app
@@ -45,13 +66,14 @@ impl App {
 fn main() {
     let application = Application::new("com.github.rust-by-example", ApplicationFlags::empty())
         .expect("Application initialization failed");
+
     application.connect_startup(|application| {
-        // type inference is available for closures but not for functions
-        let window = ApplicationWindow::new(&application);
+        let window = App::new(application.clone()).window;
         window.set_title("My first GUI");
         window.show();
         window.connect_delete_event(|_, _| Inhibit(false));
         let toolbar = Toolbar::new();
+        
         window.add(&toolbar);
         let open_button = ToolButton::new_from_stock("gtk-open");
         toolbar.add(&open_button);
@@ -80,7 +102,7 @@ fn main() {
         let quit_button = ToolButton::new_from_stock("gtk-quit");
         toolbar.add(&quit_button);
 
-        window.show_all();
+        // window.show_all();
     }); // Creates window
     application.connect_activate(|_| {});
     application.run(&env::args().collect::<Vec<_>>()); // starts the gtk event loop
