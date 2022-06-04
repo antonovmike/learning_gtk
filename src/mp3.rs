32442bab7668fcfa1b74c9ff672460ebad75f405
Mihail Antonov <antonovma@outlook.com>
2022-06-04 10:26:05 +0400

    pub struct Mp3Decoder

--- src/mp3.rs
+++ src/mp3.rs
@@ -1,12 +1,16 @@
 use std::io::{Read, Seek, SeekFrom};
 use std::time::Duration;
 
-use simplemad;
+use simplemad; // decodes frames of mpP3
 
-pub struct Mp3Decoder<R> where R: Read {
-	reader: simplemad::Decoder<R>,
-	current_frame: simplemad::Frame,
-	current_frame_channel: usize,
-	current_frame_sample_pos: usize,
-	current_time: u64,
+pub struct Mp3Decoder<R>
+// decodes frames of mpP3
+where
+    R: Read,
+{
+    reader: simplemad::Decoder<R>, // information goes from the simplemad
+    current_frame: simplemad::Frame,
+    current_frame_channel: usize,
+    current_frame_sample_pos: usize,
+    current_time: u64,
 }
