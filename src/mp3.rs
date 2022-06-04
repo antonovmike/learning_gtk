6ffb8fa88da1c95ff4adcc103ff6fbc1c0cbf241
Mihail Antonov <antonovma@outlook.com>
2022-06-04 10:39:48 +0400

    checks is it an MP3 file

--- src/mp3.rs
+++ src/mp3.rs
@@ -3,8 +3,8 @@ use std::time::Duration;
 
 use simplemad; // decodes frames of mpP3
 
-pub struct Mp3Decoder<R>
 // decodes frames of mpP3
+pub struct Mp3Decoder<R>
 where
     R: Read,
 {
@@ -13,4 +13,15 @@ where
     current_frame_channel: usize,
     current_frame_sample_pos: usize,
     current_time: u64,
+}
+
+// checks whether a stream of data is an mp3 file
+fn is_mp3<R>(mut data: R) -> bool
+where
+    R: Read + Seek,
+{
+    let stream_pos = data.seek(SeekFrom::Current(0)).unwrap();
+    let is_mp3 = simplemad::Decoder::decode(data.by_ref()).is_ok();
+    data.seek(SeekFrom::Start(stream_pos)).unwrap();
+    is_mp3
 }
